7f0700bb2296177875f0f57997625e4c86cc0568
Denis <deniskochetkov1995@gmail.com>
2023-09-11 19:17:43 +0300

    added cors

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,20 @@
-import express from 'express'
+import express from "express";
+import cors from "cors";
 
 import app from "./app";
 
 import tasksRouter from "./routers/tasks";
 
+var corsOptions = {
+  origin: "http://127.0.0.1:5500",
+};
+
 const port = 3000;
 
-app.use(express.json())
-app.use(express.urlencoded({ extended: true }))
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(cors(corsOptions))
 
 app.use("/tasks", tasksRouter);
 
